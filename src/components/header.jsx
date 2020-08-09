59bc55250a3d542d989aa016a1972078b3755a24
nikhil-tammana <nikhil.ls.tammana@gmail.com>
2020-08-09 03:11:14 -0500

    added new information

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -20,7 +20,7 @@ export class Header extends Component {
                     href="#features"
                     className="btn btn-custom btn-lg page-scroll"
                   >
-                    Learn More
+                   Learn about pillars
                   </a>{" "}
                 </div>
               </div>
