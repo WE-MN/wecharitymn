b68d6910fdf46714faa052cdf3b7ce140162fb43
pankeelshah <pankeelshahs@gmail.com>
2020-08-09 17:56:06 -0500

    removed p in events

--- src/components/events.js
+++ src/components/events.js
@@ -8,8 +8,7 @@ export class Events extends Component {
           <div className="section-title">
             <h2>Events</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-              dapibus leonec.
+
             </p>
           </div>
           <div className="row">
