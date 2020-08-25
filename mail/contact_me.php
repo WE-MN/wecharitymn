efc4d33de87e1d240129ce79ae001c7fd380fcb2
pankeelshah <pankeelshahs@gmail.com>
2020-08-25 18:17:11 -0500

    edited contact_me

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,31 +1,28 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['company'])   ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['country'])     ||
-   empty($_POST['message'])   ||
+    // check if fields passed are empty
+    if(empty($_POST['name'])        ||
+    empty($_POST['email'])      ||
+    empty($_POST['message'])    ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-{
-   echo "No arguments Provided!";
-   return false;
-}
-   
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$company = strip_tags(htmlspecialchars($_POST['company']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$country = strip_tags(htmlspecialchars($_POST['country']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'shahx317@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Contacto desde la web de parte de:  $name";
-$email_body = "Hola Verónica, recibiste un mensaje desde la planilla de contacto.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmpresa: $company\n\nEmail: $email_address\n\nTeléfono: $phone\n\nPaís: $country\n\nConsulta:\n$message";
-$headers = "From: noreply@trabajosaludable.com.ar\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+   {
+    echo "No arguments Provided!";
+    return false;
+   }
 
-$result = mail($to,$email_subject,$email_body,$headers);
-die($result);         
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+
+// create email body and send it    
+$to = 'shahx317@umn.edu'; // put your email
+$email_subject = "Contact form submitted by:  $name";
+$email_body = "You have received a new message. \n\n".
+                  " Here are the details:\n \nName: $name \n ".
+                  "Email: $email_address\n Message \n $message";
+$headers = "From: noreply@myaddress.com\n";
+
+
+$headers .= "Reply-To: $email_address"; 
+mail($to,$email_subject,$email_body,$headers);
+return true;            
 ?>
