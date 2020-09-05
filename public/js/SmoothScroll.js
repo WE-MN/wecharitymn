6cf83e4bd79170183eaebb13aaad367d7cc5c1e3
pankeelshah <pankeelshahs@gmail.com>
2020-09-04 19:09:50 -0500

    changed all http to https

--- public/js/SmoothScroll.js
+++ public/js/SmoothScroll.js
@@ -482,7 +482,7 @@ var requestFrame = (function () {
  * Viscous fluid with a pulse for part and decay for the rest.
  * - Applies a fixed force over an interval (a damped acceleration), and
  * - Lets the exponential bleed away the velocity over a longer interval
- * - Michael Herf, http://stereopsis.com/stopping/
+ * - Michael Herf, https://stereopsis.com/stopping/
  */
 function pulse_(x) {
     var val, start, expx;
--- public/js/nivo-lightbox.js
+++ public/js/nivo-lightbox.js
@@ -1,10 +1,10 @@
 /*
  * Nivo Lightbox v1.2.0
- * http://dev7studios.com/nivo-lightbox
+ * https://dev7studios.com/nivo-lightbox
  *
  * Copyright 2013, Dev7studios
  * Free to use and abuse under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
+ * https://www.opensource.org/licenses/mit-license.php
  */
 
 ;(function($, window, document, undefined){
@@ -194,15 +194,15 @@
                     classTerm = 'nivo-lightbox-video';
 
                 if(video[1] == 'youtube'){
-                    src = 'http://www.youtube.com/embed/'+ video[4];
+                    src = 'https://www.youtube.com/embed/'+ video[4];
                     classTerm = 'nivo-lightbox-youtube';
                 }
                 if(video[1] == 'youtu'){
-                    src = 'http://www.youtube.com/embed/'+ video[3];
+                    src = 'https://www.youtube.com/embed/'+ video[3];
                     classTerm = 'nivo-lightbox-youtube';
                 }
                 if(video[1] == 'vimeo'){
-                    src = 'http://player.vimeo.com/video/'+ video[3];
+                    src = 'https://player.vimeo.com/video/'+ video[3];
                     classTerm = 'nivo-lightbox-vimeo';
                 }
 
