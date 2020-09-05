2fdd13f862371189fc38c64030be05e523becc42
pankeelshah <pankeelshahs@gmail.com>
2020-09-05 17:05:21 -0500

    added statements to about section

--- src/components/about.js
+++ src/components/about.js
@@ -12,9 +12,9 @@ export class About extends Component {
                 <h2>About Us</h2>
                 <p>{this.props.data ? this.props.data.paragraph : 'loading...'}</p>
                 <h2>Our Mission</h2>
-                <div className="list-style">
-                  <p>{this.props.data ? this.props.data.Why : 'loading...'}</p>
-                </div>
+                <p>{this.props.data ? this.props.data.Why : 'loading...'}</p>
+                <a href="https://docs.google.com/document/d/13NaXXhxwC4mBFG4ud-AFEhs5zivqSZCW8GLi6fM4FMI/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><p>Our statement on the Black Lives Matter Movement.</p></a>
+                <a href="https://docs.google.com/document/d/1K6RrbZF316At9xH3r2YLyKKXI3z0-1zSWiQ-VPMf2ew/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><p>Our statement on the international WE Charity scandal.</p></a>
               </div>
             </div>
           </div>
